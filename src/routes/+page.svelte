ff9082874ba99e6f51d85570abb5603570a385cb
Bruce Kang <brucekangcn@gmail.com>
2025-05-01 18:21:06 +0800

    chore: update index appearance

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,18 +12,18 @@
 </script>
 
 <div class="m-4 mx-auto max-w-md p-2">
-    <label for="locale-selector">{m.locale()}</label>
+    <label for="locale-selector" class="label">{m.locale()}</label>
     <select id="locale-selector" bind:value={lang}>
         <option value="en">English</option>
         <option value="zh-CN">简体中文</option>
     </select>
 
-    <h2>{m.fields()}</h2>
+    <h2 class="label">{m.fields()}</h2>
 
     {#if Object.keys(fields).length > 0}
         <ul class="list">
             {#each Object.entries(fields) as [key, field] (key)}
-                <li><a href="{lang}/{key}">{field.name}</a></li>
+                <li><a href="{lang}/{key}" class="item">{field.name}</a></li>
             {/each}
         </ul>
     {:else}
@@ -34,10 +34,26 @@
 <style lang="postcss">
     @reference "tailwindcss";
 
+    .label {
+        @apply font-bold;
+        @apply whitespace-pre;
+    }
+
     .list {
         @apply flex;
         @apply flex-col;
         @apply gap-2;
         @apply p-2;
+
+        .item {
+            @apply block;
+            @apply p-2;
+            @apply border;
+            @apply shadow-md;
+
+            &:hover {
+                @apply bg-zinc-200;
+            }
+        }
     }
 </style>
