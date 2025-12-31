9ed118dde79ab32a89ed395714ca72a3a0323b92
Bruce Kang <BruceKangCN@gmail.com>
2026-01-01 06:32:13 +0800

    docs: correct comment for `splitParagraphs`

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -26,20 +26,21 @@ export function formatDate(date?: Date): string {
 }
 
 /**
- * split by 2 or more blank lines.
+ * split by blank lines.
  *
  * a blank line is a line with only whitespace characters.
  */
 export function splitParagraphs(content: string): string[] {
   // `\S\r\n` matches all non-whitespace characters plus `\r` and `\n`. `[^]`
-  // matches all characters not in the list. `[^\S\r\n]*` matches all whitespace characters
-  // except for `\r` and `\n`.
+  // matches all characters not in the list. `[^\S\r\n]*` matches all whitespace
+  // characters except for `\r` and `\n`.
   //
-  // `(\r\n|\r|\n)` find a line ending. Note that `\r\n` must be placed before
+  // `(\r\n|\r|\n)` matches a line ending. Note that `\r\n` must be placed before
   // `\r` and `\n`, otherwise they will have higher priority, causing `\r\n`
   // never being used and `\r\n` produces 2 match groups (`\r` and `\n`).
   //
-  // `[\S\r\n]*(\r\n|\r|\n)` matches a blank line. Finally, with the `{2,}`
-  // quantifier, this regular expression matches 2 to infinity blank lines.
+  // after a line ending, `[\S\r\n]*(\r\n|\r|\n)` matches a blank line. this is
+  // why the regular expression needs to match at least 2 times with the `{2,}`
+  // quantifier. the first time is for the line ending before the blank line.
   return content.split(/([^\S\r\n]*(\r\n|\r|\n)){2,}/);
 }
